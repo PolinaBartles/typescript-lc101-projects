a5bd0c1580ebe4d17ed6d181dac468c0e64b2c38
Polina Bartles <polina.bartles@gmail.com>
2023-05-03 18:25:13 -0500

    Updated Rocket file

--- studio/Rocket.ts
+++ studio/Rocket.ts
@@ -2,7 +2,7 @@ import { Cargo } from "./Cargo";
 import { Astronaut } from "./Astronaut";
 import { Payload } from "./Payload";
 
-class Rocket {
+export class Rocket {
     name: string;
     totalCapacityKg: number;
     cargoItems: Cargo[] = [];
@@ -13,7 +13,41 @@ class Rocket {
         this.totalCapacityKg = totalCapacityKg;
     }
 
+    addCargo(cargo: Cargo) : boolean {
+        if ( this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        return false;
+    }
+
+    addAustranauts(astronaut: Astronaut) : boolean {
+        if ( this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        return false;
+    }
+
+    canAdd(item: Payload): boolean {
+        return this.currentMassKg() + item.massKg <= this.totalCapacityKg
+    }
+
+    currentMassKg (): number {
+        let cargoTotalWeight = this. sumMass(this.cargoItems)
+        let astItemsTotal = this.sumMass(this.astronauts);
+        return cargoTotalWeight + astItemsTotal;
+    }
+
     sumMass(items: Payload[]): number {
-        return items.reduce((sum, item) => sum + item.massKg, 0);
+        //return items.reduce((sum, item) => sum + item.massKg, 0);
+
+        let totalWeight: number = 0;
+        for (let i = 0; i < items.length; i++) {
+            totalWeight += items[i].massKg;
+        }
+
+        return totalWeight;
       }
+
 }
